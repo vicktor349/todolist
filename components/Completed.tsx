14c7a9a189e1b1d9f537d96252ad7f3847c75393
vicktor349 <ayomidevictor349@gmail.com>
2024-09-11 10:58:56 +0100

    Completed task component

---
+++ components/Completed.tsx
@@ -0,0 +1,53 @@
+import { Checkbox } from '@mantine/core'
+import React, { useState } from 'react'
+import { IoTrashOutline } from 'react-icons/io5'
+import { MdEdit } from 'react-icons/md'
+
+
+
+const data = [
+    { id: 1, task: "Completed Task" },
+    { id: 2, task: "Completed Task" },
+    { id: 3, task: "Completed Task" },
+    { id: 4, task: "Completed Task" },
+    { id: 5, task: "Completed Task" },
+    { id: 6, task: "Completed Task" },
+    { id: 7, task: "Completed Task" },
+    { id: 8, task: "Completed Task" }
+]
+
+
+
+const Completed = () => {
+    const [completedTask, setCompletedTask] = useState(data)
+    const handleDelete = (id: number) => {
+        const updatedTask = completedTask.filter(task => task.id !== id)
+        setCompletedTask(updatedTask)
+    }
+    return (
+        <div>
+            {completedTask.map((task) => {
+                return (
+                    <div key={task.id} className='border border-gray-500 my-5'>
+                        <div className='flex items-center justify-between mx-5'>
+                            <section className='flex items-center py-3 space-x-3'>
+                                <Checkbox />
+                                <p>{task.task}</p>
+                            </section>
+                            <section className='flex items-center py-3 space-x-3'>
+                                <div className='h-7 w-7 bg-[#0085FF]  text-white flex items-center justify-center rounded-md hover:cursor-pointer'>
+                                    <MdEdit />
+                                </div>
+                                <div onClick={() => handleDelete(task.id)} className='flex items-center justify-center text-white bg-[#FF0000] rounded-md h-7 w-7 hover:cursor-pointer'>
+                                    <IoTrashOutline />
+                                </div>
+                            </section>
+                        </div>
+                    </div>
+                )
+            })}
+        </div>
+    )
+}
+
+export default Completed
